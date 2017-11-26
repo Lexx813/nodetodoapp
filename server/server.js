0b01e8b8b024ebb9d2681038d4dd4dd76c70076d
Luis Quinones <alexx813@gmail.com>
2017-11-26 15:18:47 -0500

    added test for GET /todos

--- server/server.js
+++ server/server.js
@@ -1,48 +1,46 @@
 const express = require('express');
-const bodyParser =require('body-parser');
+const bodyParser = require('body-parser');
 
-var {mongoose} =require('./db/mongoose');
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todos');
 var {User} = require('./models/user');
 
-
-const app =express();
-
+const app = express();
 
 app.use(bodyParser.json());
 
-
-
-
-///////////////////////////////////////////////
-///////////////ROUTES
-
-////////POST ROUTE
+// ///////////////////////////////////////////// /////////////ROUTES //////POST
+// ROUTE
 app.post('/todos', (req, res) => {
-   var todo = new Todo({
-       text: req.body.text
-   });
+    var todo = new Todo({text: req.body.text});
 
-   todo.save().then((doc) => {
-         res.send(doc);
-   }, (e) => {
-      res.status(400).send(e);
-   });
+    todo
+        .save()
+        .then((doc) => {
+            res.send(doc);
+        }, (e) => {
+            res
+                .status(400)
+                .send(e);
+        });
 });
-/////////////////////
-///////////GET ROUTE
-app.get('/todos', (req,res) => {
-   Todo.find().then((todos)=> {
-  res.send({todos})
-   }, (e) => {
-  res.status(400).send(e);
-   })
+///////////////////// /////////GET ROUTE
+app.get('/todos', (req, res) => {
+    Todo
+        .find()
+        .then((todos) => {
+            res.send({todos})
+        }, (e) => {
+            res
+                .status(400)
+                .send(e);
+        });
 });
-
 
 app.listen(3000, () => {
     console.log('SERVER STARTED ');
-})
+});
 
-
-module.exports= {app}
+module.exports = {
+    app
+}
